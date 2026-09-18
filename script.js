b1f2a0ff565c417d61eb8e13e6862ec26e775fc2
hossainmdsarowar464-byte <hossainmdsarowar464@gmail.com>
2026-09-19 03:50:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const list = [
   ["Premium T-Shirt",490,"1783948832_L_1.jpeg"],
-  ["Smart Watch",890,"1779024969_L_2.jpeg"],
-  ["Earbuds",690,"1779024969_L_3.jpeg"],
-  ["Travel Bag",790,"1779024969_L_4.jpeg"]
+  ["Smart Watch",890,"https://github.com/hossainmdsarowar464-byte/your-trend/blob/main/1779024969_L_2.jpeg"],
+  ["Earbuds",690,"https://github.com/hossainmdsarowar464-byte/your-trend/blob/main/1779024969_L_3.jpeg"],
+  ["Travel Bag",790,"https://github.com/hossainmdsarowar464-byte/your-trend/blob/main/1779024969_L_4.jpeg"]
 ];
 
 const box=document.getElementById("products");
