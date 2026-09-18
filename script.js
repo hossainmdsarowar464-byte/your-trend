dea9c9b33df744c87aa44e12315b8b624e739cbb
hossainmdsarowar464-byte <hossainmdsarowar464@gmail.com>
2026-09-19 02:39:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const list=[
+["Premium T-Shirt",490,"👕"],
+["Smart Watch",890,"⌚"],
+["Bluetooth Earbuds",690,"🎧"],
+["Travel Bag",790,"👜"]
+];
+
 const box=document.getElementById("products");
-const icons=["👕","⌚","🎧","👜"];
 let cart=0;
 
-for(let i=1;i<=30;i++){
-  let price=390+(i%6)*100;
+for(let i=0;i<30;i++){
+  let p=list[i%4];
   box.innerHTML+=`
   <div class="card">
-    <div class="img">${icons[i%4]}</div>
-    <h3>Premium Product ${i}</h3>
-    <p><b>৳${price}</b></p>
+    <div class="img">${p[2]}</div>
+    <h3>${p[0]}</h3>
+    <p><b>৳${p[1]}</b></p>
     <button class="btn" onclick="addCart()">Add to Cart</button>
   </div>`;
 }
@@ -16,8 +22,10 @@ for(let i=1;i<=30;i++){
 function addCart(){
   cart++;
   document.getElementById("count").innerText=cart;
-}function placeOrder(){
-  const n=name.value,p=phone.value,a=address.value;
-  if(!n||!p||!a){msg.innerText="সব তথ্য পূরণ করুন";return;}
+}
+
+function placeOrder(){
+  const n=name.value,ph=phone.value,a=address.value;
+  if(!n||!ph||!a){msg.innerText="সব তথ্য পূরণ করুন";return;}
   msg.innerText="✅ অর্ডার সফল!";
 }
