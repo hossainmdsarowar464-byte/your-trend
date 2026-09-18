fe6d41c787b2a993b7f359ef6ba9e4bb64ed0f5a
hossainmdsarowar464-byte <hossainmdsarowar464@gmail.com>
2026-09-19 02:32:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const products=document.getElementById("products");
+const box=document.getElementById("products");
 const icons=["👕","⌚","🎧","👜"];
+let cart=0;
 
 for(let i=1;i<=30;i++){
   let price=390+(i%6)*100;
-
-  products.innerHTML+=`
+  box.innerHTML+=`
   <div class="card">
     <div class="img">${icons[i%4]}</div>
     <h3>Premium Product ${i}</h3>
     <p><b>৳${price}</b></p>
-    <button class="btn">Add to Cart</button>
+    <button class="btn" onclick="addCart()">Add to Cart</button>
   </div>`;
+}
+
+function addCart(){
+  cart++;
+  document.getElementById("count").innerText=cart;
 }
