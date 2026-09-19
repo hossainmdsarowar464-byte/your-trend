2a9e736c7b7a3bda9aa02b79a014ab3f9c6d13cd
hossainmdsarowar464-byte <hossainmdsarowar464@gmail.com>
2026-09-19 12:42:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ const list = [
   {
     name: "Smart Watch",
     price: 890,
-    image: "1779024969_L_10.jpeg",
+    image: "1779024969_L_9.jpeg",
     category: "gadgets"
   },
   {
     name: "Earbuds",
     price: 690,
-    image: "1779024969_L_11.jpeg",
+    image: "1779024969_L_9.jpeg",
     category: "gadgets"
   },
   {
     name: "Travel Bag",
     price: 790,
-    image: "1779024969_L_12.jpeg",
+    image: "1779024969_L_9.jpeg",
     category: "bags"
   }
 ];
