7b8c6d0d37c5957317b603b64622b1b673acae42
hossainmdsarowar464-byte <hossainmdsarowar464@gmail.com>
2026-09-19 17:20:01 +0600

    Implement product display and cart management
    Added functionality to display product details and manage cart items.

--- product.js
+++ product.js
@@ -1,1 +1,77 @@
+const product =
+  JSON.parse(
+    localStorage.getItem("selectedProduct")
+  );
 
+
+if (!product) {
+
+  alert("⚠️ Product পাওয়া যায়নি");
+
+  window.location.href = "index.html";
+
+}
+
+
+document.getElementById("productImage").src =
+  product.image;
+
+
+document.getElementById("productName").innerText =
+  product.name;
+
+
+document.getElementById("productPrice").innerText =
+  product.price;
+
+
+document.getElementById("productDescription").innerText =
+  product.description;
+
+
+function addSelectedProduct() {
+
+  let cartItems =
+    JSON.parse(
+      localStorage.getItem("yourTrendCart")
+    ) || [];
+
+
+  const existing =
+    cartItems.find(function (item) {
+
+      return item.name === product.name;
+
+    });
+
+
+  if (existing) {
+
+    existing.quantity++;
+
+  } else {
+
+    cartItems.push({
+
+      name: product.name,
+
+      price: product.price,
+
+      image: product.image,
+
+      quantity: 1
+
+    });
+
+  }
+
+
+  localStorage.setItem(
+    "yourTrendCart",
+    JSON.stringify(cartItems)
+  );
+
+
+  alert("✅ Product Cart-এ যোগ হয়েছে");
+
+}
